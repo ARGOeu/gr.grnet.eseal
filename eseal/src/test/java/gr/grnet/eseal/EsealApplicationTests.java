81b11f2ca7041cbe0aeb249d1cd715f871f2a97d
agelostsal <agelos.tsal@gmail.com>
2021-02-17 22:01:26 +0200

    ET-53 Introduce configuration properties

--- eseal/src/test/java/gr/grnet/eseal/EsealApplicationTests.java
+++ eseal/src/test/java/gr/grnet/eseal/EsealApplicationTests.java
@@ -1,10 +1,31 @@
 package gr.grnet.eseal;
 
+import gr.grnet.eseal.config.HaricaProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 class EsealApplicationTests {
+
+	private HaricaProperties haricaProperties;
+
+	@Autowired
+	 EsealApplicationTests(HaricaProperties haricaProperties) {
+		this.haricaProperties = haricaProperties;
+	}
+
+	@Test
+	void testHaricaPropertiesLoad() {
+		assertThat("test.harica.com").isEqualTo(this.haricaProperties.getEndpoint());
+		assertThat("test-user").isEqualTo(this.haricaProperties.getUsername());
+		assertThat("test-password").isEqualTo(this.haricaProperties.getPassword());
+		assertThat("test-key").isEqualTo(this.haricaProperties.getKey());
+	}
 
 	@Test
 	void contextLoads() {
