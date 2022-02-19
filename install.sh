bff3097d3b3ddb982eaac871fef5b2651aa484cd
Zhukov Andreas <ZhukovA@unisystems.gr>
2022-02-19 14:19:19 +0200

    smallfixes for logging to work properly on centos

---
+++ install.sh
@@ -0,0 +1,19 @@
+# login as unisys
+mkdir /opt/eseal
+mkdir /opt/eseal/log
+cp eseal-0.0.1-SNAPSHOT.jar /opt/eseal
+cp eseal-0.0.1-SNAPSHOT.conf /opt/eseal
+cp eseal.service /opt/eseal
+chmod +x eseal-0.0.1-SNAPSHOT.jar
+
+# to override default logging - there is already logback.xml and logback jars inside the executable JAR
+# https://docs.spring.io/spring-boot/docs/2.1.8.RELEASE/reference/html/howto-logging.html
+cp logback.xml /opt/eseal
+
+# by this point program should work, if executed via shell: ./eseal-0.0.1-SNAPSHOT.jar
+# logs should be written to /opt/eseal/log/eseal.log - not sure but maybe they are even rotated
+
+# https://docs.spring.io/spring-boot/docs/current/reference/html/deployment.html#deployment.installing.nix-services.system-d
+cp eseal.service /etc/systemd/system
+# maybe should restart systemctl daemon?
+systemctl enable myapp.service
