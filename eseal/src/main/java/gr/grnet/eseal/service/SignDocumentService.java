3eb9a5b2295f1efdeb9d4089066ad5adc99c2ee5
Zhukov Andreas <ZhukovA@unisystems.gr>
2022-10-27 12:38:44 +0300

    removed the discrepancy with github code in SignDocumentService when passing image - changed osddydd code to work according to github

--- eseal/src/main/java/gr/grnet/eseal/service/SignDocumentService.java
+++ eseal/src/main/java/gr/grnet/eseal/service/SignDocumentService.java
@@ -3,6 +3,7 @@ package gr.grnet.eseal.service;
 import eu.europa.esig.dss.enumerations.SignerTextHorizontalAlignment;
 import eu.europa.esig.dss.enumerations.VisualSignatureAlignmentHorizontal;
 import eu.europa.esig.dss.enumerations.VisualSignatureAlignmentVertical;
+import eu.europa.esig.dss.model.InMemoryDocument;
 import eu.europa.esig.dss.model.x509.CertificateToken;
 import eu.europa.esig.dss.pades.SignatureImageParameters;
 import eu.europa.esig.dss.pades.SignatureImageTextParameters;
@@ -34,6 +35,7 @@ import java.util.function.BiFunction;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 
 public interface SignDocumentService {
@@ -225,9 +227,11 @@ public interface SignDocumentService {
     signatureImageParameters.setTextParameters(signatureImageTextParameters);
 
     // check if an image has been provided, otherwise use the default
-    // 20220523: provided images are not used anymore, a hardcoded single image is used for
-    // everything - NB: this is a difference that shouldn't be merged from devel GRNET branch
-    signatureImageParameters.setImage(visibleSignatureProperties.getImageDocument());
+    if (StringUtils.isNotEmpty(imageBytes)) {
+      signatureImageParameters.setImage(new InMemoryDocument(Utils.fromBase64(imageBytes)));
+    } else {
+      signatureImageParameters.setImage(visibleSignatureProperties.getImageDocument());
+    }
 
     switch (position) {
       case TOP_LEFT:
